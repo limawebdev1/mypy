0892d07db5aa6bc64179c0b30aa812428e18805c
Lisa Ma <limawebdev@gmail.com>
2017-06-04 15:35:04 -0600

    setting server up

--- hello/helloapp/helloapp/urls.py
+++ hello/helloapp/helloapp/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('howdy.urls'))
+    url(r'^', include('howdy.urls')),
+    url(r'^servers/', include('servers.urls')),
 ]
